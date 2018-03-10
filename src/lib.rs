c6835282c285fac276fbcafa774ba89710763c50
Yi Zhang <yiz@metrumrg.com>
2018-03-10 00:36:33 -0500

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@
 //!  Hence the crate is meant to be experimental.
 //!
 //! Below is how to do the problem example in section 2.1 of
-//! `Bob Carpenter, Matthew D. Hoffman, Marcus Brubaker, Daniel Lee, Peter Li, and Michael J. Betancourt. 2015. The Stan Math Library: Reverse-Mode Automatic Differentiation in C++. arXiv 1509.07164`, using `dorset`.
+//! the Stan Math paper(Bob Carpenter, Matthew D. Hoffman, Marcus Brubaker, Daniel Lee, Peter Li, and Michael J. Betancourt. 2015. The Stan Math Library: Reverse-Mode Automatic Differentiation in C++. arXiv 1509.07164),
+//! using `dorset`.
 //! ```
 //! #[macro_use]
 //! extern crate dorset;
@@ -20,18 +21,11 @@
 //!     lp = &lp - ln(&sigma);
 //!     lp = &lp - 0.5 * pow((y - &mu) / &sigma, 2.0);
 //!     lp.grad();
-//!     println!("f(mu, sigma) = {:?}", lp.val());
-//!     println!("d.f/d.mu = {:?}", mu.adj());
-//!     println!("d.f/d.sigma = {:?}", sigma.adj());
+//!     println!("f(mu, sigma) = {0:.6}", lp.val()); // f(mu, sigma) = -1.323482
+//!     println!("d.f/d.mu = {0:.6}", mu.adj());     // d.f/d.mu = 0.555556
+//!     println!("d.f/d.sigma = {0:.6}", sigma.adj()); // d.f/d.sigma = -0.462963
 //! }
 //!
-//! ```
-//! The output from `cargo run` of the above code is
-//!
-//! ```
-//! f(mu, sigma) = -1.323482
-//! d.f/d.mu = 0.555556
-//! d.f/d.sigma = -0.462963
 //! ```
 
 extern crate typed_arena;
